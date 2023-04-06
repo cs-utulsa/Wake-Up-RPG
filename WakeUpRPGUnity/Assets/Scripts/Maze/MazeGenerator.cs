9728b27edf7bc833d061e61bc9e3fb654be471c3
Pilar Carrera <carremonpi2@gmail.com>
2023-04-06 12:41:01 -0600

    maze and menu

--- WakeUpRPGUnity/Assets/Scripts/Maze/MazeGenerator.cs
+++ WakeUpRPGUnity/Assets/Scripts/Maze/MazeGenerator.cs
@@ -24,7 +24,7 @@ public class MazeGenerator : MonoBehaviour {
     public GameObject roomPrefab;
     public GameObject goalPrefab;
     public GameObject playerGO;
-    private GameObject[,] maze;
+    [SerializeField] private GameObject[,] maze;
     System.Random rand = new System.Random();
 
 
